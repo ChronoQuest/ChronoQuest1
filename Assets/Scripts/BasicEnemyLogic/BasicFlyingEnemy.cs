72dc04221cdb232f041a32f511334630b5435842
ethanr04 <na23961@bristol.ac.uk>
2026-02-05 09:25:21 +0000

    Integrated new changes into branch

--- Assets/Scripts/BasicEnemyLogic/BasicFlyingEnemy.cs
+++ Assets/Scripts/BasicEnemyLogic/BasicFlyingEnemy.cs
@@ -71,7 +71,7 @@ void Update()
         else
         {
             // If enemy has awoken, increase detection range
-            if (currentState == State.Sleeping) if (currentState == State.Sleeping)
+            if (currentState == State.Sleeping) detectionRange += (float)2;
             currentState = State.Chase;
         }
 
