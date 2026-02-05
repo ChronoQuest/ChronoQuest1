c4f945ec54c6700b226fe32963104721be35974b
Anthony <anthony@brandr.com>
2026-02-05 13:06:37 +0000

    Refactor: reusable TrapDamage component, remove damage from SwingingBladeTrap

--- Assets/Cainos/Pixel Art Platformer - Dungeon/Script/SwingingBladeTrap.cs
+++ Assets/Cainos/Pixel Art Platformer - Dungeon/Script/SwingingBladeTrap.cs
@@ -11,10 +11,6 @@ namespace Cainos.PixelArtPlatformer_Dungeon
         public float bladeRotationMaxAngle = 50.0f;
         public float bladeRotationTime = 3.0f;
 
-        [Header("Damage Settings")]
-        [Tooltip("Amount of damage dealt to the player on contact")]
-        public int damageAmount = 1;
-
         [Header("Objects")]
         public Transform blade;
 
@@ -29,28 +25,6 @@ namespace Cainos.PixelArtPlatformer_Dungeon
             v = bladeRotationCurve.Evaluate(timer / bladeRotationTime);
 
             blade.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, v * bladeRotationMaxAngle);
-        }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            // Check if the colliding object is the player
-            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
-            if (playerHealth != null)
-            {
-                // Deal damage (negative value for damage)
-                playerHealth.ModifyHealth(-damageAmount);
-            }
-        }
-
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            // Also check for regular collisions in case a Collider2D is used instead of a trigger
-            PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
-            if (playerHealth != null)
-            {
-                // Deal damage (negative value for damage)
-                playerHealth.ModifyHealth(-damageAmount);
-            }
         }
     }
 }
