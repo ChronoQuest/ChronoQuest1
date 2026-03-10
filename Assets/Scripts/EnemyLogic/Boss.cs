956e7758a1bbb0314c32d8f5d4e8b54fe30b4c3e
ethanr04 <na23961@bristol.ac.uk>
2026-03-10 09:32:59 +0000

    Removed debugging from boss script

--- Assets/Scripts/EnemyLogic/Boss.cs
+++ Assets/Scripts/EnemyLogic/Boss.cs
@@ -211,7 +211,6 @@ public class Boss : EnemyBase, IRewindable
             else if (rand < 0.5f) currentRes = ResMove.FireWave;
             else if (rand < 0.75f) currentRes = ResMove.Platforms;
             else currentRes = ResMove.Enemy;
-            currentRes = ResMove.Platforms;
         }
     void UpdateCombat()
     {
