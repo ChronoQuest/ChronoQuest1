776ca9b6f9bd90fe4039cdc140f7f71b3fa66579
Megan Parfitt <za23370@bristol.ac.uk>
2026-04-14 13:31:31 +0100

    preprocessing file, gmm notebook, early stopping in neural network, dropout, saving model details

--- Assets/Scripts/Final Boss ML/NeuralBossModel.py
+++ Assets/Scripts/Final Boss ML/NeuralBossModel.py
@@ -11,66 +11,19 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import KFold
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout 
+from tensorflow.keras.callbacks import EarlyStopping
+from preprocessing import preprocess 
 
 EPOCHS = 30
 BATCH_SIZE = 16 
 FINAL_EPOCHS = 15 
 FINAL_BATCH_SIZE = 8
-# TODO maybe add RUN_EXPERIMENTS = False
 
-# ===== LOAD DATA =====
-# TODO potentially move to preprocessing file as used for both models
-def get_data_path():
-    system = platform.system()
+X_scaled, scaler, feature_names = preprocess()
 
-    if system == "Darwin":
-        return Path.home() / "Library" / "Application Support" / "DefaultCompany" / "ChronoQuest1"
-    elif system == "Windows":
-        return Path.home() / "AppData" / "LocalLow" / "DefaultCompany" / "ChronoQuest1"
-
-data_folder = get_data_path()
-data_path = data_folder / "session_data.jsonl"
-
-df = pd.read_json(data_path, lines=True)
-
-# getting rid of randomness on each run of model
 tf.random.set_seed(42)
 np.random.seed(42)
-
-
-# ===== FEATURE ENGINEERING =====
-# filtering out shorter player sessions
-df = df[df["session_duration_seconds"] > 30]  
-
-df["dash_rate"] = df["dash_count"] / df["session_duration_seconds"]
-df["jump_rate"] = (df["jump_count"] + df["wall_jump_count"] + df["double_jump_count"]) / df["session_duration_seconds"]
-df["melee_rate"] = df["melee_attacks"] / df["session_duration_seconds"]
-df["spell_rate"] = df["spell_casts"] / df["session_duration_seconds"]
-df["rewind_rate"] = df["rewind_activation_count"] / df["session_duration_seconds"]
-df["damage_rate"] = df["damage_taken_total"] / df["session_duration_seconds"]
-
-df["melee_accuracy"] = df["melee_hits"] / (df["melee_attacks"] + 1e-5)
-df["spell_accuracy"] = df["spell_hits"] / (df["spell_casts"] + 1e-5)
-
-features = [
-    "dash_rate",
-    "jump_rate",
-    "melee_rate",
-    "spell_rate",
-    "rewind_rate",
-    "damage_rate",
-    "melee_accuracy",
-    "spell_accuracy"
-]
-
-X = df[features]
-
-
-# ===== SCALE FEATURES =====
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-print("Length of X Scaled: ", len(X_scaled))
-
 
 # ===== CREATING LABELS =====
 gmm = GaussianMixture(n_components=5, covariance_type="diag", random_state=42)
@@ -150,7 +103,6 @@ model = Sequential([
     Dense(len(np.unique(y)), activation='softmax')
 ])
 
-# TODO how do we choose the best optimiser and loss function
 model.compile(
     optimizer='adam',
     loss='sparse_categorical_crossentropy',
@@ -216,9 +168,13 @@ for batch_size in batch_sizes:
 
 
 # ===== FINAL MODEL ===== 
+callback = EarlyStopping(monitor='val_loss', patience=5, min_delta=0.001, restore_best_weights=True)
+
 model = Sequential([
     Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
+    # Dropout(0.2),
     Dense(16, activation='relu'),
+    # Dropout(0.2),
     Dense(len(np.unique(y)), activation='softmax')
 ])
 
@@ -230,16 +186,21 @@ model.compile(
 
 history = model.fit(
     X_train2, y_train2,
-    epochs=FINAL_EPOCHS,
+    epochs=50,
     batch_size=FINAL_BATCH_SIZE,
     validation_data=(X_val, y_val),
-    shuffle=False,
+    shuffle=True,
+    callbacks=[callback],
     verbose=1
 )
 
-
 # ===== EVALUATION AND METRICS =====
 # metrics output are related to the final model
+print("\nTraining stopped at epoch: ", len(history.history['loss']))
+
+stopped_epoch = callback.stopped_epoch
+print("\nStopped at epoch:", stopped_epoch + 1)
+
 loss, acc = model.evaluate(X_test, y_test)
 print("\nTest Accuracy:", acc)
 
@@ -290,4 +251,19 @@ folds_results = {
 with open(folds_dir / "folds_results.json", "w") as f:
     json.dump(folds_results, f, indent=4)
 
-# TODO save model for unity
+# saving model to .json file for unity
+model_data = {
+    "weights": [w.tolist() for w in model.get_weights()],
+    "scaler_mean": scaler.mean_.tolist(),
+    "scaler_scale": scaler.scale_.tolist(),
+    "input_size": X_train.shape[1],
+    "output_size": len(np.unique(y))
+}
+
+unity_path = Path(__file__).resolve().parents[3] / "Assets" / "StreamingAssets"
+unity_path.mkdir(parents=True, exist_ok=True)
+
+model_path = unity_path / "neural_network_model.json"
+
+with open(model_path, "w") as f:
+    json.dump(model_data, f)
