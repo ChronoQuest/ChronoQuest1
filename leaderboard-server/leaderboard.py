613a8b25ba9594e565711a693b308d62f5c53d78
Megan Parfitt <za23370@bristol.ac.uk>
2026-04-19 20:13:42 +0100

    ...again

--- leaderboard-server/leaderboard.py
+++ leaderboard-server/leaderboard.py
@@ -11,8 +11,16 @@ FILE = "leaderboard.json"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = FastAPI()
-templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
-app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+'''templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")''' 
+
+templates = Jinja2Templates(directory="leaderboard-server/templates")
+
+app.mount(
+    "/static",
+    StaticFiles(directory="leaderboard-server/static"),
+    name="static"
+)
 
 leaderboard = []
 
@@ -40,14 +48,14 @@ def save_scores(data):
         json.dump(data, f, indent=4) '''
 
 # --- routes --- 
-''' @app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse(name="index.html", context={"request": request}) '''
+    return templates.TemplateResponse(name="index.html", context={"request": request}) 
 
-@app.get("/")
+'''@app.get("/")
 def test():
     import os
-    return {"files": os.listdir()}
+    return {"files": os.listdir()}'''
 
 @app.get("/leaderboard")
 def get_leaderboard():
