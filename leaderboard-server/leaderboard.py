638ceaefc12f5d3c4e006a610440046792a81b4c
Megan Parfitt <za23370@bristol.ac.uk>
2026-04-19 19:52:24 +0100

    another fix

--- leaderboard-server/leaderboard.py
+++ leaderboard-server/leaderboard.py
@@ -8,10 +8,11 @@ import json
 import os
 
 FILE = "leaderboard.json"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name='static')
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 
 leaderboard = []
 
